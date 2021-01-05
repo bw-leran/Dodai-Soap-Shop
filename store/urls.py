612ce5cf392df18faeaa8eec85680e93a86763cc
bw-leran <brandonmillerut@gmail.com>
2021-01-04 16:21:55 -1000

    Initial commit

---
+++ store/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+#maybe need these later?
+#from django.contrib import admin
+#from django.urls import path
+
+urlpatterns = [
+    path('',views.store,name='store'),
+    path('cart/',views.cart,name='cart'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('about/',views.about,name='about'),
+    path('update_item/',views.updateItem,name='update_item'),
+    path('process_order/',views.processOrder,name='process_order'),
+]
